9d22bb62238d4af944a0d49fc92d73c9f6db6e27
zaz7 <abdullah75farid@gmail.com>
2024-07-11 13:49:41 +0300

    quoe

--- project-1.js
+++ project-1.js
@@ -6,4 +6,34 @@ let  quote = document.querySelector(".quote");
 
 let person = document.querySelector(".person") ;
 
-const quote
+const quotes = [{
+    quote:`"The only way to do great work is to love what you do."`,
+    person: `Steve Jobs`
+},{
+    quote:`"Be yourself; everyone else is already taken."`,
+    person: `Oscar Wilde`
+},{
+    quote:`"In the end, it's not the years in your life that count. It's the life in your years."`,
+    person: `Abraham Lincoln`
+},{
+    quote:`"The greatest glory in living lies not in never falling, but in rising every time we fall."`,
+    person: `Nelson Mandela`
+},{
+    quote:`"Success is not final, failure is not fatal: It is the courage to continue that counts."`,
+    person: `Winston Churchill`
+},{
+    quote:`"The future belongs to those who believe in the beauty of their dreams."`,
+    person: `Steve Jobs`
+},{
+    quote:`"The only way to do great work is to love what you do."`,
+    person: `Steve Jobs`
+},{
+    quote:`"The only way to do great work is to love what you do."`,
+    person: `Steve Jobs`
+},{
+    quote:`"The only way to do great work is to love what you do."`,
+    person: `Steve Jobs`
+},{
+    quote:`"The only way to do great work is to love what you do."`,
+    person: `Steve Jobs`
+}]
