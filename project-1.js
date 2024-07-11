a125fc0582d256eb858327e95457a113ae9c6862
zaz7 <abdullah75farid@gmail.com>
2024-07-11 15:24:58 +0300

    edefhjekjfer

--- project-1.js
+++ project-1.js
@@ -40,7 +40,7 @@ const quotes = [{
 
 
 btn.addEventListener("click", function(){
-    let random = Math.floor(Math.random * quotes.length());
+    let random = Math.floor(Math.random() * quotes.length);
 
     quote.innerText = quotes[random].quote; 
     person.innerText = quotes[random].person;
